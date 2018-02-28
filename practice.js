fc28de5f75cc92be80135d24f64c8e6ce1cc3d2a
Kyle Clawson <kyleclawson@Kyles-MacBook-Pro.local>
2018-02-28 09:47:19 -0700

    a little more extra

--- practice.js
+++ practice.js
@@ -117,10 +117,10 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
-function divider(numbersArray){
-  for (i=0;)
+// function divider(numbersArray){
+//   for (i=0);
 
-}
+// }
 
 
 ////////// PROBLEM 7 //////////
